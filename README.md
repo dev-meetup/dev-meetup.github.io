06a23ba4e99424ac4e954b45562975abccc83f00
anseonghyun <sh84.ahn@gmail.com>
2017-02-26 15:22:05 +0900

    add jsonschema testcase

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 ### 개발 관련 링크
 - [bootstarap](getbootstrap.com)
 - [bootstarap materialkit](https://www.creative-tim.com/)
+     - [material color](https://material.io/guidelines/style/color.html#color-color-palette)
 - [fullcalendar](https://fullcalendar.io)
 - [React](https://facebook.github.io/react/)
+
+
 
 
 
---
+++ test/test.py
@@ -0,0 +1,26 @@
+import unittest
+
+
+class JsonValidateCase(unittest.TestCase):
+
+    def setUp(self):
+        json_data_file_path = './data/events.json'
+        json_schema_file_path = './test/json-schema.json'
+
+        import json
+        self.json_data = json.loads(self.get_json_from_file(json_data_file_path))
+        self.json_schema = json.loads(self.get_json_from_file(json_schema_file_path))
+
+    def get_json_from_file(self, path):
+        with open(path, 'r') as f:
+            return f.read()
+
+    def test_data_json(self):
+        from jsonschema import validate, ValidationError
+        try:
+            validate(self.json_data, self.json_schema)
+        except Exception as e:
+            self.fail(str(e))
+
+if __name__ == '__main__':
+    unittest.main()
